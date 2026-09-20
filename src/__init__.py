491d792cac9ada04cf7630e574611ff5aec4c1af
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-09-20 17:05:32 -0600

    Set margin default to 0

--- src/__init__.py
+++ src/__init__.py
@@ -75,7 +75,7 @@ def register():
         default='CYLINDER',
     )
     bpy.types.Scene.rk_turnaround_margin = bpy.props.FloatProperty(
-        name = "Margin", default = 1.1, min = 0.0, max = 3.0,
+        name = "Margin", default = 0.0, min = 0.0, max = 3.0,
     )
     
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
